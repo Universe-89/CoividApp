be564365c33b624cacc22a2710bfdae7d7da4e53
tanmayks1999 <tanmayks1999.ts@gmail.com>
2021-04-14 20:47:31 +0530

    register

--- locations/urls.py
+++ locations/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path('getLocation/',views.currentLocation.as_view(), name="getCurrentLocation"),
-path('<str:room_name>/', views.location, name='location'),
+    path('getLocation/',views.currentLocation.as_view(), name="getCurrentLocation"),
+    path('<str:room_name>/', views.location, name='location'),
 ]
