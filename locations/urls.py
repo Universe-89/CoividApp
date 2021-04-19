0f29f07a9725edeeec2cee1663ddd1943affca42
tanmayks1999 <tanmayks1999.ts@gmail.com>
2021-04-20 05:09:45 +0530

    qrcode

--- locations/urls.py
+++ locations/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('getLocation/',views.currentLocation.as_view(), name="getCurrentLocation"),
+    path('qrcode/',views.qrcode, name="getCurrentLocation"),
     path('<str:room_name>/', views.location, name='location'),
 ]
